7d158b09021d371bea21ac6d46da9ea420bd6fe1
davidcoats <david@getsafetycone.com>
2023-02-05 22:06:20 -0800

    Initial commit.

---
+++ source/R5T.NG0018.Construction/Code/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace R5T.NG0018.Construction
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.NG0018/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.NG0018
+{
+	/// <summary>
+	/// Microsoft.AspNetCore.Components.WebAssembly NuGet package selector.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
